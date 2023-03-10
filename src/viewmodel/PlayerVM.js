6c7ef084a446edb6ca0521330292f4ce9ead4239
Krista M <morales-k@outlook.com>
2023-03-09 20:48:02 -0500

    Updated PlayerVM
    Added movement function, and passed playerWidth var for consistency.

--- src/viewmodel/PlayerVM.js
+++ src/viewmodel/PlayerVM.js
@@ -1,16 +1,38 @@
 /**
  * Draws a rectangle with it's origin begining at updatedPlayerX.
  * 
- * @param {object} ctx - The context of the canvas object.
- * @param {number} updatedPlayerX - X coordinate of the mouse.
+ * @param {Object} ctx - The context of the canvas object.
+ * @param {Number} playerX - The X coordinate of the player.
+ * @param {Number} updatedPlayerX - X coordinate of the mouse.
  */
-export const drawPlayer = (ctx, updatedPlayerX) => {
+export const drawPlayer = (ctx, playerWidth, updatedPlayerX) => {
   ctx.beginPath();
-  ctx.rect(updatedPlayerX, 75, 70, 70);
+  ctx.rect(updatedPlayerX, 75, playerWidth, playerWidth);
   ctx.fillStyle = 'blue';
   ctx.fill();
   ctx.stroke(); 
 };
+
+/**
+ * Calculates & returns the current player position.
+ * 
+ * @param {Number} maxWidth - The width of the canvas bounding rect.
+ * @param {Number} playerWidth - The width of the player.
+ * @param {Number} playerX - Origin of the x-coord of the player.
+ * @param {object} arrowStates - Object indicating if left or right arrow key is currently pressed.
+ * @returns 
+ */
+export function calculatePlayerMovement(maxWidth, playerWidth, playerX, arrowStates) {
+  let updatedXCoord = playerX;
+
+  if (!!arrowStates.leftArrow && playerX >= 0) {
+      updatedXCoord = playerX -= 10;
+  } else if (!!arrowStates.rightArrow && playerX <= maxWidth - playerWidth) {
+      updatedXCoord = playerX += 10;
+  }
+  
+  return updatedXCoord;
+}
 
 /**
 * Updates coordinates & detects canvas edge.
@@ -24,6 +46,7 @@ export function trackPlayer(playerWidth, playerX, canvas) {
   const rect = canvas.current.getBoundingClientRect();
   const leftEdgeCollision = playerX <= 0 ? true : false;
   const rightEdgeCollision = playerX >= rect.width - playerWidth ? true : false;
+
   let updatedPlayerX = 0;
 
   // Reset coords if mouse touches edge.
@@ -36,25 +59,4 @@ export function trackPlayer(playerWidth, playerX, canvas) {
   }
 
   return updatedPlayerX;
-}
-
-/**
-* Calculates & returns the current player position.
-* 
-* @param {Number} maxWidth - The width of the canvas bounding rect.
-* @param {Number} playerWidth - The width of the player.
-* @param {Number} playerX - Origin of the x-coord of the player.
-* @param {object} arrowStates - Object indicating if left or right arrow key is currently pressed.
-* @returns 
-*/
-export function calculatePlayerX(maxWidth, playerWidth, playerX, arrowStates) {
-  let updatedPosX = playerX;
-
-  if (!!arrowStates.leftArrow && playerX >= 0) {
-      updatedPosX = playerX -= 10;
-  } else if (!!arrowStates.rightArrow && playerX <= maxWidth - playerWidth) {
-      updatedPosX = playerX += 10;
-  }
-  
-  return updatedPosX;
 }
