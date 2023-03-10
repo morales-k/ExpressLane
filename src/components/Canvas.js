4ea96b1c6627ecf0891034e8d3d91879e3339430
Krista M <morales-k@outlook.com>
2023-03-09 19:10:38 -0500

    Moved canvas logic to VM
    Updated event listeners and added arrow state object to handle tracking.

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,16 +1,11 @@
 import React, {useState, useEffect, useRef} from 'react'
-import { setupCanvas, draw } from '../viewmodel/CanvasVM';
+import { handleEvent, setupCanvas, draw, calculatePlayerMovement } from '../ViewModel/CanvasVM';
 import ArrowKeys from './ArrowKeys';
 
 function Canvas() {
-  const centerWidth = document.getElementById('gameboard').offsetWidth / 2;
-  const centerHeight = document.getElementById('gameboard').offsetHeight / 2;
   const orientation = window.matchMedia("(orientation: portrait)");
   const [canvasReady, setCanvasReady] = useState(false);
-  const [canvasCenterX, setCanvasCenterX] = useState(centerWidth);
-  const [canvasCenterY, setCanvasCenterY] = useState(centerHeight);
   const [isPortrait, setPortrait] = useState(orientation.matches);
-  const [playerX, setplayerX] = useState(100);
   const canvas = useRef();
 
   // Set up listeners
@@ -23,47 +18,36 @@ function Canvas() {
       setPortrait(orientation.matches);
     });
 
+    window.addEventListener('keydown', (e) => calculatePlayerMovement(e));
+    window.addEventListener('keyup', (e) => calculatePlayerMovement(e));
+
     // Clean up
     return () => {
       window.removeEventListener('resize', () => {
         setupCanvas(canvas, setCanvasReady);
         setPortrait(orientation.matches);
       });
+
+      window.removeEventListener('keydown', (e) => calculatePlayerMovement(e));
+      window.removeEventListener('keyup', (e) => calculatePlayerMovement(e));
     };
   }, []);
 
   useEffect(() => {
     if (canvasReady) {
-      draw(canvas, playerX);
+      draw(canvas);
     }
   }, [canvasReady]);
 
-  // Moves player based on arrow keys. tabIndex MUST be focused(0) for onKeyDown event to fire.
-  function movePlayer(e) {
-    if (e.target.innerText === '←' || e.key === 'ArrowLeft') {
-      if (playerX < 25) { // Prevent negative X coordiate.
-        setplayerX(0);
-      } else {
-        setplayerX(playerX - 25);
-      }
-    } else if (e.target.innerText === '→' || e.key === 'ArrowRight') {
-      if (playerX + 95 >= window.screen.availWidth) { // 95 is used because the player is 70 pixels wide & moving by 25 pixels.
-        setplayerX(window.screen.availWidth - 70);
-      } else {
-        setplayerX(playerX + 25);
-      }
-    }
-  }
-
   return (
     <>
-      <ArrowKeys 
-        movePlayer={movePlayer} />
+      {/* <ArrowKeys 
+        calculatePlayerMovement={calculatePlayerMovement} /> */}
       <canvas 
         id="canvas" 
         ref={canvas} 
         tabIndex={0} 
-        onKeyDown={(e) => movePlayer(e)} />
+        onKeyDown={(e) => handleEvent(e)} />
     </>
   )
 }
--- src/viewmodel/CanvasVM.js
+++ src/viewmodel/CanvasVM.js
@@ -1,5 +1,11 @@
-import { car } from "../assets/canvasObjects";
+import { trackPlayer, calculatePlayerX, drawPlayer } from "./PlayerVM";
 const dpr = window.devicePixelRatio || 1;
+let playerX = 100;
+let playerWidth = 70;
+let arrowStates = {
+  leftArrow: false,
+  rightArrow: false,
+};
 
 /**
  * Scales canvas by dpr to fix blur and sets canvas ready state.
@@ -26,13 +32,50 @@ export function setupCanvas(canvas, setCanvasReady) {
  * @param {object} canvas - The canvas element itself.
  * @param {number} playerX - The player's X coord.
  */
-export function draw(canvas, playerX) {
+export function draw(canvas) {
     const ctx = canvas.current.getContext('2d');
     const rect = canvas.current.getBoundingClientRect();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     canvas.width = rect.width * dpr;
     canvas.height = rect.height * dpr;
 
-    car(ctx, playerX);
+    const currentPlayerX = calculatePlayerX(rect.width, 75, playerWidth, arrowStates);
+    const updatedPlayerX = trackPlayer(75, currentPlayerX, canvas);
+
+    drawPlayer(ctx, updatedPlayerX);
     requestAnimationFrame(() => draw(canvas, playerX));
+}
+
+/**
+ * Handles user events.
+ * 
+ * @param {object} e - The event object.
+ */
+export function handleEvent(e) {
+  if (e.key === "ArrowLeft") {
+       e.type === "keyup" ? arrowStates.leftArrow = false : arrowStates.leftArrow = true;
+  } else if (e.key === "ArrowRight") {
+      e.type === "keyup" ? arrowStates.rightArrow = false : arrowStates.rightArrow = true;
+  }
+}
+
+// Moves player based on arrow keys. tabIndex MUST be focused(0) for onKeyDown event to fire.
+export function calculatePlayerMovement(playerX) {
+  let updatedXCoord = playerX;
+
+  if (arrowStates.leftArrow) {
+    if (playerX < 25) { // Prevent negative X coordiate.
+      updatedXCoord = 0;
+    } else {
+      updatedXCoord = playerX - 25;
+    }
+  } else if (arrowStates.rightArrow) {
+    if (playerX + 95 >= window.screen.availWidth) { // 95 is used because the player is 70 pixels wide & moving by 25 pixels.
+      updatedXCoord = window.screen.availWidth - 70;
+    } else {
+      updatedXCoord = playerX + 25;
+    }
+  }
+
+  return updatedXCoord;
 }
