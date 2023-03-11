0e4f3665f21fbaddcf4d47d9923556dd53cc878f
Krista M <morales-k@outlook.com>
2023-03-10 21:21:46 -0500

    Updated Player Tests
    Added expectations that player stays within bounds. Wrapped in describe.

--- src/components/__tests__/Player.test.js
+++ src/components/__tests__/Player.test.js
@@ -4,32 +4,36 @@ import Canvas from "../Canvas";
 import { handleEvent, arrowStates } from "../../ViewModel/CanvasVM";
 import { calculatePlayerMovement } from "../../ViewModel/PlayerVM";
 
-test("Player moves left when left arrow key is pressed.", () => {
-  render(<Canvas />);
-  const maxWidth = 100;
-  const playerWidth = 70;
-  const playerX = 30;
-  const leftArrowPressed = new KeyboardEvent("keydown", {"key": "ArrowLeft"});
-  const rightArrowUp = new KeyboardEvent("keyup", {"key": "ArrowRight"});
-
-  handleEvent(rightArrowUp);
-  handleEvent(leftArrowPressed);
-  const updatedXCoord = calculatePlayerMovement(maxWidth, playerWidth, playerX, arrowStates);
-
-  expect(updatedXCoord).toBeLessThan(playerX);
-});
-
-test("Player moves right when right arrow key is pressed.", () => {
-  render(<Canvas />);
-  const maxWidth = 100;
-  const playerWidth = 70;
-  const playerX = 20;
-  const leftArrowUp = new KeyboardEvent("keyup", {"key": "ArrowLeft"});
-  const rightArrowPressed = new KeyboardEvent("keydown", {"key": "ArrowRight"});
-
-  handleEvent(leftArrowUp);
-  handleEvent(rightArrowPressed);
-  const updatedXCoord = calculatePlayerMovement(maxWidth, playerWidth, playerX, arrowStates);
-
-  expect(updatedXCoord).toBeGreaterThan(playerX);
+describe("Player moves left/right depending on key press, without going out of bounds.", () => {
+  test("Player moves left.", () => {
+    render(<Canvas />);
+    const maxWidth = 100;
+    const playerWidth = 70;
+    const playerX = 30;
+    const leftArrowPressed = new KeyboardEvent("keydown", {"key": "ArrowLeft"});
+    const rightArrowUp = new KeyboardEvent("keyup", {"key": "ArrowRight"});
+  
+    handleEvent(rightArrowUp);
+    handleEvent(leftArrowPressed);
+    const updatedXCoord = calculatePlayerMovement(maxWidth, playerWidth, playerX, arrowStates);
+  
+    expect(updatedXCoord).toBeLessThan(playerX);
+    expect(updatedXCoord).toBeGreaterThanOrEqual(0);
+  });
+  
+  test("Player moves right.", () => {
+    render(<Canvas />);
+    const maxWidth = 100;
+    const playerWidth = 70;
+    const playerX = 20;
+    const leftArrowUp = new KeyboardEvent("keyup", {"key": "ArrowLeft"});
+    const rightArrowPressed = new KeyboardEvent("keydown", {"key": "ArrowRight"});
+  
+    handleEvent(leftArrowUp);
+    handleEvent(rightArrowPressed);
+    const updatedXCoord = calculatePlayerMovement(maxWidth, playerWidth, playerX, arrowStates);
+  
+    expect(updatedXCoord).toBeGreaterThan(playerX);
+    expect(updatedXCoord).toBeLessThanOrEqual(maxWidth - playerWidth);
+  });
 });
