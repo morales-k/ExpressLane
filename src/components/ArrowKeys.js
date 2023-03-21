82148ed97292baa196658131054718f51672c57e
Krista M <morales-k@outlook.com>
2023-03-21 19:04:20 -0400

    Simplified ArrowKeys
    Created array to map each button & used image background for styling.

--- src/components/ArrowKeys.js
+++ src/components/ArrowKeys.js
@@ -1,31 +1,27 @@
 import React from 'react';
-import leftarrow from "../Assets/leftarrow.png";
-import rightarrow from "../Assets/rightarrow.png";
-import uparrow from "../Assets/uparrow.png";
-import downarrow from "../Assets/downarrow.png";
+import { handleEvent } from '../ViewModel/CanvasVM';
 
 function ArrowKeys() {
+  const directions = ["Left", "Up", "Down", "Right"];
 
   return (
     <div className="arrow-keys-container">
-      <span>
-        <button type="button" className="arrow">
-          <img src={leftarrow} alt="Left arrow" />
-        </button>
-      </span>
-      <span>
-        <button type="button" className="arrow">
-          <img src={uparrow} alt="Up arrow" />
-        </button>
-        <button type="button" className="arrow">
-          <img src={downarrow} alt="Down arrow" />
-        </button>
-      </span>
-      <span>
-        <button type="button" className="arrow">
-          <img src={rightarrow} alt="Right arrow" />
-        </button>
-      </span>
+
+      {
+        directions && directions.map(dir => {
+          return (
+            <span key={`arrow${dir}Span`}>
+              <button 
+                type="button" 
+                className="arrow"
+                id={`arrow${dir}`} 
+                onMouseDown={(e) => handleEvent(e)}
+                onMouseUp={(e) => handleEvent(e)}>
+              </button>
+            </span>
+          );
+        })
+      }
     </div>
   )
 }
